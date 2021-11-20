e5dd5e4832b1cd77a3a961dbb063ad3435900d26
Dogan Teke <teke900@gmail.com>
2021-11-20 19:15:12 +0100

    adds new register page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (true) {
-        //     URL::forceScheme('https');
-        // }
+        if (true) {
+            URL::forceScheme('https');
+        }
     }
 }
